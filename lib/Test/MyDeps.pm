93c057866da3c5ee94afee32b89ac7c4e40f6daf
Dave Rolsky <autarch@urth.org>
2009-12-12 15:11:00 -0600

    Bump version

--- lib/Test/MyDeps.pm
+++ lib/Test/MyDeps.pm
@@ -3,7 +3,7 @@ package Test::MyDeps;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 # CPAN::Reporter spits out random output we don't want, and we don't want to
 # report these tests anyway.
