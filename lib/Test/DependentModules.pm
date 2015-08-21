3950361a856670b49908481d5e20520a0c2a99ed
Dave Rolsky <autarch@urth.org>
2015-08-21 17:42:01 -0500

    Bump version after release

--- lib/Test/DependentModules.pm
+++ lib/Test/DependentModules.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use autodie;
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 # CPAN::Reporter spits out random output we don't want, and we don't want to
 # report these tests anyway.
