789699f1a9709efe877bce13a3c31b46f0b60b17
Dave Rolsky <autarch@urth.org>
2019-01-09 15:17:38 -0600

    Remove POD sections which are now auto-generated

--- lib/Test/DependentModules.pm
+++ lib/Test/DependentModules.pm
@@ -654,28 +654,4 @@ directory must already exist.
 You also can enable CPAN's output by setting the
 C<$ENV{PERL_TEST_DM_CPAN_VERBOSE}> variable to a true value.
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-test-mydeps@rt.cpan.org>,
-or through the web interface at L<http://rt.cpan.org>.  I will be notified,
-and then you'll automatically be notified of progress on your bug as I make
-changes.
-
-=head1 DONATIONS
-
-If you'd like to thank me for the work I've done on this module, please
-consider making a "donation" to me via PayPal. I spend a lot of free time
-creating free software, and would appreciate any support you'd care to offer.
-
-Please note that B<I am not suggesting that you must do this> in order for me
-to continue working on this particular software. I will continue to do so,
-inasmuch as I have in the past, for as long as it interests me.
-
-Similarly, a donation made in this way will probably not make me work on this
-software much more, unless I get so many donations that I can consider working
-on free software full time, which seems unlikely at best.
-
-To donate, log into PayPal and send money to autarch@urth.org or use the
-button on this page: L<http://www.urth.org/~autarch/fs-donation.html>
-
 =cut
